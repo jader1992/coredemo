7a1116fe0054bfd6aa0a1df95532e256f8783260
peiyu <1249200310@qq.com>
2021-12-13 02:33:35 +0800

    增加优雅关闭

--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"context"
 	"gocore/framework"
 	"gocore/framework/middleware"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 func main() {
@@ -24,8 +29,29 @@ func main() {
 		Addr: ":8888",
 	}
 
-	err := server.ListenAndServe()
-	if err != nil {
-		log.Println(err)
+	go func() {
+		server.ListenAndServe()
+	}()
+
+	// 当前的goroutine等待信号量
+	quit := make(chan os.Signal)
+
+	// 因为使用 Ctrl 或者 kill 命令，它们发送的信号是进入 main 函数的，即只有 main 函数所在的 Goroutine 会接收到，
+	// 所以必须在 main 函数所在的 Goroutine 监听信号。
+	//  关注的监控信号：SIGINT, SIGTERM, SIGQUIT
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+	// 这里会阻塞当前goroutine等待信号
+	<-quit
+
+	// 调用Server.Shutdown graceful结束，控制最多等待的时间
+	timeoutCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	// server.Shutdown方法是个阻塞方法，一旦执行之后，他会阻塞当前的goroutine，并且在所有连接请求结束后，才继续往后执行
+	// 这种方式与grace优雅关闭服务比较相似
+	// 内部实现：是通过双层循环：第一层循环是定时无限循环，每过ticker的间隔时间，就进入第二层循环；第二层循环是遍历连接中的所有请求，
+	// 如果已经处理完操作处于 Idle 状态，就关闭连接，直到所有连接都关闭，才返回。
+	if err := server.Shutdown(timeoutCtx); err != nil {
+		log.Fatal("server shutdown:", err)
 	}
 }
--- user_controller.go
+++ user_controller.go
@@ -1,8 +1,12 @@
 package main
 
-import "gocore/framework"
+import (
+	"gocore/framework"
+	"time"
+)
 
 func UserLoginController(c *framework.Context) error {
+	time.Sleep(10 * time.Second)
 	c.SetOkStatus().Json("ok, UserLoginController")
 	return nil
 }
