b36611dd29f758236cb4b37b92b96f69f858547d
peiyu <1249200310@qq.com>
2021-12-18 23:24:41 +0800

    自动安装中间件cors & 修改路由

--- app/http/route.go
+++ app/http/route.go
@@ -1,11 +1,11 @@
 package http
 
 import (
-    "github.com/jader1992/gocore/app/http/middleware/cors"
-    "github.com/jader1992/gocore/app/http/module/demo"
+	"github.com/jader1992/gocore/app/http/middleware/cors"
+	"github.com/jader1992/gocore/app/http/module/demo"
 	"github.com/jader1992/gocore/framework/gin"
 	"github.com/jader1992/gocore/framework/middleware"
-  "github.com/jader1992/gocore/framework/middleware/static"
+	"github.com/jader1992/gocore/framework/middleware/static"
 )
 
 // Routes 绑定业务层路由
@@ -13,11 +13,11 @@ func Routes(r *gin.Engine) {
 
 	// 设置静态服务
 	//r.Static("/dist/", "./dist/")
-  r.Use(static.Serve("/", static.LocalFile("./dist", false)))
+	r.Use(static.Serve("/", static.LocalFile("./dist", false)))
 
 	// 使用链路追踪中间件
 	r.Use(middleware.Trace())
-    // 使用cors中间件
-    r.Use(cors.Default())
+	// 使用cors中间件
+	r.Use(cors.Default())
 	demo.Register(r) // 注册路由
 }
